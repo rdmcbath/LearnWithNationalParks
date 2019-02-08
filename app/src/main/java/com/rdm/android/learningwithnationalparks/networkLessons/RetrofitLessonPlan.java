fbb735abec83ca68861c9f9e1f271a895e3df1ff
Rebecca McBath <rdmcbath@outlook.com>
2019-02-08 15:00:52 -0500

    Update RetrofitLessonPlan.java

--- app/src/main/java/com/rdm/android/learningwithnationalparks/networkLessons/RetrofitLessonPlan.java
+++ app/src/main/java/com/rdm/android/learningwithnationalparks/networkLessons/RetrofitLessonPlan.java
@@ -7,12 +7,12 @@ public interface RetrofitLessonPlan {
 
     //This call executes the lesson plan search, sorting by subject, and retrieves them from the
     //National Park Service API. Here is what the query looks like:
-    //https://developer.nps.gov/api/v1/lessonplans?sort=subject&api_key=(MYAPIKEYHERE)
+    //https://developer.nps.gov/api/v1/lessonplans?sort=subject&api_key=[MYAPIKEY]
 
-    @GET("lessonplans?sort=subject&api_key=ykqtU0ep0jqNDHp2TcNfXDzO7pKQMznBZsJk0kEu")
+    @GET("lessonplans?sort=subject&api_key=[MYAPIKEY]")
     Call<LessonPlan> getLessonPlanSubject();
 
-    @GET("lessonplans?sort=title&api_key=ykqtU0ep0jqNDHp2TcNfXDzO7pKQMznBZsJk0kEu")
+    @GET("lessonplans?sort=title&api_key=[MYAPIKEY]")
     Call<LessonPlan> getLessonPlanTitle();
 }
 
