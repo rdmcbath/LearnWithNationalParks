7ae7461b9650c395534c710d59d5e496a6c65a12
Rebecca McBath <rdmcbath@outlook.com>
2019-03-28 15:09:38 -0400

    Added onSwipeRefresh to Images page

--- app/src/main/java/com/rdm/android/learningwithnationalparks/fragments/ImageGridFragment.java
+++ app/src/main/java/com/rdm/android/learningwithnationalparks/fragments/ImageGridFragment.java
@@ -9,6 +9,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -39,133 +40,150 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class ImageGridFragment extends Fragment {
-    private static final String LOG_TAG = ImageGridFragment.class.getSimpleName();
+import static com.rdm.android.learningwithnationalparks.R.drawable.custom_progress_spinner;
 
-    @BindView(R.id.image_grid_recycler)
-    RecyclerView mImageGridRecyclerView;
-    @BindView(R.id.images_frame_layout)
-    FrameLayout frameLayout;
-    @BindView(R.id.progress_bar)
-    ProgressBar progressBar;
-    @BindView(R.id.empty_view)
-    @Nullable
-    TextView mEmptyView;
+public class ImageGridFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+	private static final String LOG_TAG = ImageGridFragment.class.getSimpleName();
 
-    private List<FlickrPhoto> photoItems = new ArrayList<>();
-    private FlickrResponse flickrResponse;
-    private Description description;
-    private Context context;
-    private static final String LIST_IMPORT = "image_grid";
-    private Parcelable mListState;
-    private LinearLayoutManager mLayoutManager;
-    private ImageAdapter mImageAdapter;
+	@BindView(R.id.image_grid_recycler)
+	RecyclerView mImageGridRecyclerView;
+	@BindView(R.id.images_frame_layout)
+	FrameLayout frameLayout;
+	@BindView(R.id.progress_bar)
+	ProgressBar progressBar;
+	@BindView(R.id.empty_view)
+	@Nullable
+	TextView mEmptyView;
+	@BindView(R.id.swipe_refresh_layout)
+	SwipeRefreshLayout refreshLayout;
 
-    public ImageGridFragment() {
-    }
+	private List<FlickrPhoto> photoItems = new ArrayList<>();
+	private FlickrResponse flickrResponse;
+	private Description description;
+	private Context context;
+	private static final String LIST_IMPORT = "image_grid";
+	private Parcelable mListState;
+	private LinearLayoutManager mLayoutManager;
+	private ImageAdapter mImageAdapter;
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        super.onViewCreated(container, savedInstanceState);
-        setHasOptionsMenu(true);
+	public ImageGridFragment() {
+	}
 
-        if (savedInstanceState != null) {
-            photoItems = savedInstanceState.getParcelableArrayList(LIST_IMPORT);
-        }
-        View rootView = inflater.inflate(R.layout.fragment_images, container, false);
-        ButterKnife.bind(this, rootView);
-        mImageGridRecyclerView.setVisibility(View.GONE);
-        progressBar.setVisibility(View.VISIBLE);
+	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		super.onViewCreated(container, savedInstanceState);
+		setHasOptionsMenu(true);
 
-        boolean mDualPane = getResources().getBoolean(R.bool.is_tablet);
+		if (savedInstanceState != null) {
+			photoItems = savedInstanceState.getParcelableArrayList(LIST_IMPORT);
+		}
+		View rootView = inflater.inflate(R.layout.fragment_images, container, false);
+		ButterKnife.bind(this, rootView);
+		mImageGridRecyclerView.setVisibility(View.GONE);
+		progressBar.setVisibility(View.VISIBLE);
 
-        if (mDualPane) {
-            mLayoutManager = new GridLayoutManager(getContext(), 4);
-        } else {
-            mLayoutManager = new GridLayoutManager(getContext(), 2);
-        }
-        mImageGridRecyclerView.setLayoutManager(mLayoutManager);
-        mImageAdapter = new ImageAdapter(photoItems, description, getActivity());
-        mImageGridRecyclerView.setAdapter(mImageAdapter);
+		refreshLayout.setOnRefreshListener(this);
+		refreshLayout.setColorSchemeResources(R.color.category_images);
 
-        // Get a reference to the ConnectivityManager to check state of network connectivity
-        ConnectivityManager connMgr = (ConnectivityManager)
-                Objects.requireNonNull(getContext()).getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = null;
-        if (connMgr != null) {
-            networkInfo = connMgr.getActiveNetworkInfo();
-        }
-        // If there is a network connection, load lesson plan data
-        if (networkInfo != null && networkInfo.isConnected()) {
+		boolean mDualPane = getResources().getBoolean(R.bool.is_tablet);
 
-            loadImageGrid();
-            Log.d(LOG_TAG, "loadImageGrid Method Called");
+		if (mDualPane) {
+			mLayoutManager = new GridLayoutManager(getContext(), 4);
+		} else {
+			mLayoutManager = new GridLayoutManager(getContext(), 2);
+		}
+		mImageGridRecyclerView.setLayoutManager(mLayoutManager);
+		mImageAdapter = new ImageAdapter(photoItems, description, getActivity());
+		mImageGridRecyclerView.setAdapter(mImageAdapter);
 
-        } else {
-            Snackbar snackbar = Snackbar
-                    .make(frameLayout, R.string.no_network, Snackbar.LENGTH_SHORT);
-            snackbar.show();
-        }
-        return rootView;
-    }
+		// Get a reference to the ConnectivityManager to check state of network connectivity
+		ConnectivityManager connMgr = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 
-    private void loadImageGrid() {
-        RetrofitFlickr retrofitFlickr = FlickrClient.getClient().create(RetrofitFlickr.class);
-        Call<FlickrResponse> call = retrofitFlickr.getParkPlacesGroupPhotostream();
-        Log.d(LOG_TAG, "RetrofitFlickr - GetPhotostream Called");
+		NetworkInfo networkInfo = null;
+		if (connMgr != null) {
+			networkInfo = connMgr.getActiveNetworkInfo();
+		}
+		// If there is a network connection, load lesson plan data
+		if (networkInfo != null && networkInfo.isConnected()) {
 
-        call.enqueue(new Callback<FlickrResponse>() {
+			loadImageGrid();
+			Log.d(LOG_TAG, "loadImageGrid Method Called");
 
-            @Override
-            public void onResponse(Call<FlickrResponse> call, Response<FlickrResponse> response) {
+		} else {
+			Snackbar snackbar = Snackbar
+					.make(frameLayout, R.string.no_network, Snackbar.LENGTH_SHORT);
+			snackbar.show();
+		}
+		return rootView;
+	}
 
-                if (response.isSuccessful()) {
-                    FlickrResponse flickrResponse = response.body();
-                    photoItems = flickrResponse.getPhotos().getPhotoItems();
-                    Log.d(LOG_TAG, "Response.Body Retrofit Called");
-                    mImageAdapter = new ImageAdapter(photoItems, description, getContext());
-                    mImageGridRecyclerView.setAdapter(mImageAdapter);
-                    mLayoutManager.onRestoreInstanceState(mListState);
-                    progressBar.setVisibility(View.GONE);
-                    mImageGridRecyclerView.setVisibility(View.VISIBLE);
-                    mImageAdapter.notifyDataSetChanged();
+	private void loadImageGrid() {
+		RetrofitFlickr retrofitFlickr = FlickrClient.getClient().create(RetrofitFlickr.class);
+		Call<FlickrResponse> call = retrofitFlickr.getParkPlacesGroupPhotostream();
+		Log.d(LOG_TAG, "RetrofitFlickr - GetPhotostream Called");
 
-                } else {
-                    System.out.println(response.errorBody());
-                    if (mEmptyView != null) {
-                        mEmptyView.setText(R.string.no_images_found);
-                    }
-                }
-            }
+		call.enqueue(new Callback<FlickrResponse>() {
 
-            @Override
-            public void onFailure(Call<FlickrResponse> call, Throwable t) {
-                t.printStackTrace();
-                if (mEmptyView != null) {
-                    mEmptyView.setText(R.string.no_network);
-                }
-            }
-        });
-    }
+			@Override
+			public void onResponse(Call<FlickrResponse> call, Response<FlickrResponse> response) {
 
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putParcelableArrayList(LIST_IMPORT, (ArrayList<? extends Parcelable>) photoItems);
-    }
+				if (response.isSuccessful()) {
+					FlickrResponse flickrResponse = response.body();
+					photoItems = flickrResponse.getPhotos().getPhotoItems();
+					Log.d(LOG_TAG, "Response.Body Retrofit Called");
+					mImageAdapter = new ImageAdapter(photoItems, description, getContext());
+					mImageGridRecyclerView.setAdapter(mImageAdapter);
+					mLayoutManager.onRestoreInstanceState(mListState);
+					progressBar.setVisibility(View.GONE);
+					mImageGridRecyclerView.setVisibility(View.VISIBLE);
+					mImageAdapter.notifyDataSetChanged();
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        mImageAdapter.notifyDataSetChanged();
-    }
+				} else {
+					System.out.println(response.errorBody());
+					if (mEmptyView != null) {
+						mEmptyView.setText(R.string.no_images_found);
+					}
+				}
+			}
 
-    public void setFlickrPhoto(FlickrPhoto flickrPhoto) {
-    }
+			@Override
+			public void onFailure(Call<FlickrResponse> call, Throwable t) {
+				t.printStackTrace();
+				if (mEmptyView != null) {
+					mEmptyView.setText(R.string.no_network);
+				}
+			}
+		});
+	}
 
-    public void setPhotos(Photos photos) {
-    }
+	@Override
+	public void onSaveInstanceState(@NonNull Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putParcelableArrayList(LIST_IMPORT, (ArrayList<? extends Parcelable>) photoItems);
+	}
 
-    public void setFlickrPhoto(List<FlickrPhoto> photoItems) {
-        this.photoItems = photoItems;
-    }
+	@Override
+	public void onResume() {
+		super.onResume();
+		mImageAdapter.notifyDataSetChanged();
+	}
+
+	@Override
+	public void onRefresh() {
+		loadImageGrid();
+		refreshDone();
+	}
+
+	private void refreshDone() {
+		refreshLayout.setRefreshing(false);
+	}
+
+	public void setFlickrPhoto(FlickrPhoto flickrPhoto) {
+	}
+
+	public void setPhotos(Photos photos) {
+	}
+
+	public void setFlickrPhoto(List<FlickrPhoto> photoItems) {
+		this.photoItems = photoItems;
+	}
 }
